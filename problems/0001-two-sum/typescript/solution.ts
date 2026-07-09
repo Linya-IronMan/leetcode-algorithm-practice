5e4a5028201362810360a9ddc0782c55744d5c1e
陈定涛 <chendingtao@reredata.com>
2026-07-09 09:23:48 +0800

    refactor(0001-two-sum): 优化 TypeScript 解法，使用 map.get 代替 has 并增加边界说明

--- problems/0001-two-sum/typescript/solution.ts
+++ problems/0001-two-sum/typescript/solution.ts
@@ -1,11 +1,10 @@
 export function twoSum(nums: number[], target: number): number[] {
-    const map = new Map<number, number>();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement)!, i];
-        }
-        map.set(nums[i], i);
-    }
-    return [];
+	let map = new Map<number, number>();
+	for (let i = 0; i < nums.length; i++) {
+		const n1 = nums[i];
+		const n2 = target - n1;
+		if (map.get(n2) === undefined) map.set(n1, i);
+		else return [map.get(n2)!, i];
+	}
+	return [];
 }
