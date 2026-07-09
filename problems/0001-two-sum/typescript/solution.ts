06adebfc7293982857e2077debf9b009582ef0a7
陈定涛 <chendingtao@reredata.com>
2026-07-09 09:27:22 +0800

    Optimize twoSum lookup using map.has

--- problems/0001-two-sum/typescript/solution.ts
+++ problems/0001-two-sum/typescript/solution.ts
@@ -3,7 +3,7 @@ export function twoSum(nums: number[], target: number): number[] {
 	for (let i = 0; i < nums.length; i++) {
 		const n1 = nums[i];
 		const n2 = target - n1;
-		if (map.get(n2) === undefined) map.set(n1, i);
+		if (!map.has(n2)) map.set(n1, i);
 		else return [map.get(n2)!, i];
 	}
 	return [];
