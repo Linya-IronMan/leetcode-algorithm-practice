9cf14b79ab46b00666c155bceac8d0a57c0bc48e
陈定涛 <chendingtao@reredata.com>
2026-07-14 08:36:04 +0800

    feat: rust 手写 leetcode 1

--- problems/0001-two-sum/rust/src/lib.rs
+++ problems/0001-two-sum/rust/src/lib.rs
@@ -3,14 +3,16 @@ use std::collections::HashMap;
 pub struct Solution;
 
 impl Solution {
-    pub fn twoSum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-        for (i, &num) in nums.iter().enumerate() {
-            let complement = target - num;
-            if let Some(&index) = map.get(&complement) {
-                return vec![index as i32, i as i32];
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for (i, item) in nums.iter().enumerate() {
+            let n1 = item;
+            let n2 = target - n1;
+            if let Some(i3) = map.get(&n2) {
+                return vec![*i3 as i32, i as i32];
+            } else {
+                map.insert(*item, i as i32);
             }
-            map.insert(num, i);
         }
         vec![]
     }
@@ -29,13 +31,13 @@ mod tests {
 
     #[test]
     fn test_all() {
-        let content = std::fs::read_to_string("../testcases.json")
-            .expect("Failed to read testcases.json");
-        let cases: Vec<TestCase> = serde_json::from_str(&content)
-            .expect("Failed to parse testcases.json");
+        let content =
+            std::fs::read_to_string("../testcases.json").expect("Failed to read testcases.json");
+        let cases: Vec<TestCase> =
+            serde_json::from_str(&content).expect("Failed to parse testcases.json");
         for case in cases {
             // TODO: 根据题目的输入参数和返回类型修改这里的解包与 assert
-            assert_eq!(Solution::twoSum(case.input.0, case.input.1), case.output);
+            assert_eq!(Solution::two_sum(case.input.0, case.input.1), case.output);
         }
     }
 }
